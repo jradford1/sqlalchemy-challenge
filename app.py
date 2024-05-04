b8160e6be681988a5b5316e1178130bb0e288c6f
jradford1 <152581989+jradford1@users.noreply.github.com>
2024-05-04 18:31:56 +1000

    app.py started

--- app.py
+++ app.py
@@ -1,5 +1,11 @@
 # Import the dependencies.
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine
+from sqlalchemy import func
 
+from flask import Flask, jsonify
 
 
 #################################################
@@ -7,18 +13,23 @@
 #################################################
 
 # Create engine using the `hawaii.sqlite` database file
-
+engine = create_engine("sqlite:///hawaii.sqlite", echo=False)
 # Declare a Base using `automap_base()`
+Base = automap_base()
+
 
 # Use the Base class to reflect the database tables
-
+Base.prepare(autoload_with=engine)
 
 # Assign the measurement class to a variable called `Measurement` and
 # the station class to a variable called `Station`
+#Measurement class assigned to variable called Measurement
+Measurement = Base.classes.measurement
 
-
+#Station class assigned to a variable called Station
+Station = Base.classes.station
 # Create a session
-
+session = Session(engine)
 
 #################################################
 # Flask Setup
